87f58c09e3375b9d76280cff28ba5dadbee6e7ed
Bert <bert.k@hotmail.com>
2024-03-13 08:58:42 +0100

    update .NET SDK version (#349)
    Signed-off-by: Bert <bert.k@hotmail.com>

--- src/DotNetDev.ArcadeLight.Sdk.Tests/CheckRequiredDotNetVersionTests.cs
+++ src/DotNetDev.ArcadeLight.Sdk.Tests/CheckRequiredDotNetVersionTests.cs
@@ -25,7 +25,7 @@ namespace DotNetDev.ArcadeLight.Sdk.Tests
     }
 
     [Theory]
-    [InlineData("8.0.201", true)]
+    [InlineData("8.0.202", true)]
     [InlineData("8.0.888", true)]
     public void CheckRequiredDotNetVersionVerify(string minSdkVersionStr, bool expectedResult)
     {
